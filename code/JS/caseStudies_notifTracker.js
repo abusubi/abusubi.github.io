9b4b69a3bb2bd2277ea0db1323ceb9fc4b241b3e
abusubi <asimkhan23@gmail.com>
2021-09-28 15:57:15 -0500

    move if else statement

--- code/JS/caseStudies_notifTracker.js
+++ code/JS/caseStudies_notifTracker.js
@@ -29,13 +29,13 @@ function amountscrolled(){
         || document.body.parentNode || document.body).scrollTop;
     // gets percentage scrolled (ie: 80 or NaN if tracklength == 0);
     var pctScrolled = Math.floor(scrollTop/trackLength * 100);
-    console.log(pctScrolled + '% scrolled');
-    if (pctScrolled => 75){
-        console.log('Case study marked as visited-');
-    }
-    else {
-      // do nothing
-    }
+}
+
+if (amountscrolled() => 75){
+    console.log('Case study marked as visited-');
+}
+else {
+  // do nothing
 }
 
 getmeasurements();
