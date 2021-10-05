f2eb846926af94ceed25d1a5a17106abbd8492ee
abusubi <asimkhan23@gmail.com>
2021-10-05 16:46:33 -0500

    92 querySelectorAll(".highlighted-color.color-yellow'");

--- code/JS/caseStudies_notifTracker.js
+++ code/JS/caseStudies_notifTracker.js
@@ -89,6 +89,7 @@ var onceSlideNotif = once(
 //SET UP VIEWPORT SELECTOR
 // Get the color-yellow text.
 var yellowThanks = document.getElementsByClassName('highlighted-color color-yellow');
+var yellowThanks = document.querySelectorAll(".highlighted-color.color-yellow'");
 // Get it's position in the viewport
 var bounding = yellowThanks.getBoundingClientRect();
 console.log(yellowThanks);
