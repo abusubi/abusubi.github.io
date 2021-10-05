a608f67799053092bfb24d61674ab7b8f5fc4d5a
abusubi <asimkhan23@gmail.com>
2021-10-04 20:42:05 -0500

    Remove DOMContentLoaded and title function. Did not work.

--- code/JS/caseStudies_notifTracker.js
+++ code/JS/caseStudies_notifTracker.js
@@ -127,10 +127,3 @@ window.addEventListener("scroll", function(){
 
 
 }, false)
-
-// INSERT NOTIFICATION VARIABLES
-//  document.getElementById("cases_remaining").innerHTML = notifPrint + ' Cases Remaining';
-
-document.addEventListener("DOMContentLoaded", function(){
-  document.getElementById("page_title").innerHTML = document.title;
-})
