a7c272b2a565d58673f6417a20def1eda1f349de
abusubi <asimkhan23@gmail.com>
2021-11-11 19:23:57 -0600

    changed timeout function to await delay.

--- code/JS/caseStudies_notifTracker.js
+++ code/JS/caseStudies_notifTracker.js
@@ -30,7 +30,7 @@ var onceSlideNotif = once(
 		console.log('In the viewport!');
 		localStorage.setItem('visited-'+window.location.pathname, 'visitedtrue');
   	document.getElementById("notif_container").className = "slide-in";
-  	setTimeout(slideOut, 5500);
+		await delay(slideOut, 5500);
   }
 );
 
